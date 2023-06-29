d723ef870a8fd4ce3fa6cee3a396be30a1e3efa8
TheAstutian <speakingatoms@gmail.com>
2023-06-30 00:03:56 +0100

    light styling Nav.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,10 +4,11 @@ import './Navigation.css';
 const Navigation =()=>{
     return(
         <div className='navigation-container'>
-        <div className=''>
+        <div className='admin'>
+            <p>Pweshy Logo</p>
             <p>Admin: Pwesh</p> 
         </div>
-        <div>
+        <div className='navigation'>
             <ul>
                 <li>Home</li>
                 <li>Contact Me</li>
