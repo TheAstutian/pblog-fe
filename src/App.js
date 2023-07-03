76bf14a0a2a12f85a63753da5608d6408a19b01c
TheAstutian <speakingatoms@gmail.com>
2023-07-03 23:01:07 +0100

    Working on blog component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation.js';
 import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer'
-import Blog from './components/Blog/Blog';
+
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Home />
-      <Blog />
       <Footer />
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import Blog from '../Blog/Blog';
 
-function Home(){
-   return(
-    <>
-    <p>This is the hero area</p>
-    <p>This is the blog care display area</p>
-    </>
-   )
+class Home extends React.Component{
+    state={
+        user:{},
+        blog_entries:[ 
+            {
+                title:"Lorem Ipsum",
+                body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            },
+            {title:"Title 2",
+            body:"Body2"
+        }
+        ]
+    }
+
+
+    render(){
+        return(
+            <>
+            <div>
+            <p>This is the hero area</p>
+            </div>
+
+            <div>
+            <p>This is the blog care display area</p>    
+            <Blog title={this.state.blog_entries[1].title}
+                    body={this.state.blog_entries[1].body}
+            />
+            </div>
+
+            </>
+           )
+        
+    }
 }
 
 export default Home; 
